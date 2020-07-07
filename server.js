c423fd1a65be61bf407d213a492caa84c827f147
Conner Schiller <conner.schiller@gmail.com>
2020-07-07 08:03:30 -0700

    added all route connections to database

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const { response } = require('express');
 
-const pg = knex({
+const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
@@ -13,8 +14,6 @@ const pg = knex({
       database : 'smart-brain'
     }
 });
-
-console.log(pg.select().from('users'));
 
 const app = express();
 app.use(express.json());
@@ -53,68 +52,90 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select().from('users').where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0]);
+            })
+            .catch(err => res.status(400).json('unable to get user'))
         } else {
-            res.status(400).json('error');
+            response.status(400).json('wrong credentials');
         }
-    res.json('this is working');
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users').returning('*').insert({
+                    name: name,
+                    email: loginEmail[0],
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('unable to register'));
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    db.select().from('users').where({
+        id: id
+    })
+    .then(user => {
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json('user not found');
         }
     })
-    if (!found) {
-        res.status(404).send('user not found');
-    }
+    .catch (err => {
+        res.status(400).json('error getting user');
+    })
 })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-    if (!found) {
-        res.status(404).send('user not found');
-    }
+    .catch(err => {
+        res.status(400).json('unable to get entries');
+    })
 })
 
-bcrypt.hash("cookies", null, null, function(err, hash) {
-    console.log(hash);
-});
+// bcrypt.hash("cookies", null, null, function(err, hash) {
+//     console.log(hash);
+// });
 
-// Load hash from your password DB.
-bcrypt.compare("cookies", "$2a$10$XDQNwRgoQRo1n4u3xU/EMOqvIRnVGTPMYipcQ7VxHMZL3Uf2h2pTm", function(err, res) {
-    console.log(res);
-});
-bcrypt.compare("veggies", "$2a$10$XDQNwRgoQRo1n4u3xU/EMOqvIRnVGTPMYipcQ7VxHMZL3Uf2h2pTm", function(err, res) {
-    console.log(res);
-});
+// // Load hash from your password DB.
+// bcrypt.compare("cookies", "$2a$10$XDQNwRgoQRo1n4u3xU/EMOqvIRnVGTPMYipcQ7VxHMZL3Uf2h2pTm", function(err, res) {
+//     console.log(res);
+// });
+// bcrypt.compare("veggies", "$2a$10$XDQNwRgoQRo1n4u3xU/EMOqvIRnVGTPMYipcQ7VxHMZL3Uf2h2pTm", function(err, res) {
+//     console.log(res);
+// });
 
 app.listen(PORT, () => {
     console.log('Listening on port:', PORT);
