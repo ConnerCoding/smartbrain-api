63ee8a06200fc944d79994c76c25bbfe9d926b9c
Conner Schiller <conner.schiller@gmail.com>
2020-07-08 13:24:21 -0700

    removed original dummy db and extra bcrypt syntax examples

--- server.js
+++ server.js
@@ -21,33 +21,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 3000;
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'Sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
 
 app.get('/', (req, res) => {
     res.send(database.users);
@@ -86,18 +59,6 @@ app.put('/image', (req, res) => {
         res.status(400).json('unable to get entries');
     })
 })
-
-// bcrypt.hash("cookies", null, null, function(err, hash) {
-//     console.log(hash);
-// });
-
-// // Load hash from your password DB.
-// bcrypt.compare("cookies", "$2a$10$XDQNwRgoQRo1n4u3xU/EMOqvIRnVGTPMYipcQ7VxHMZL3Uf2h2pTm", function(err, res) {
-//     console.log(res);
-// });
-// bcrypt.compare("veggies", "$2a$10$XDQNwRgoQRo1n4u3xU/EMOqvIRnVGTPMYipcQ7VxHMZL3Uf2h2pTm", function(err, res) {
-//     console.log(res);
-// });
 
 app.listen(PORT, () => {
     console.log('Listening on port:', PORT);
