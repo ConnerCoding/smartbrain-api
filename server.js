df0aadcc5308d1b43863f5121dfede77a9ef4609
Conner Schiller <conner.schiller@gmail.com>
2020-08-14 18:45:16 -0700

    added comments to server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// Connect to postgresql db using knex
 const db = knex({
     client: 'pg',
     connection: {
@@ -17,11 +18,14 @@ const db = knex({
     }
 });
 
+// Start express app and middlewares
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
+
+// Routes/API
 app.get('/', (req, res) => res.send('It is running'))
 app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt))
 app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt))
